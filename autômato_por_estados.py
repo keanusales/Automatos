9d9bcb8b89daa2a98f0e9b1b84484e964a2ace27
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2022-12-05 14:03:09 -0300

    Update autômato_por_estados.py

--- autômato_por_estados.py
+++ autômato_por_estados.py
@@ -5,14 +5,10 @@ LETTERS = "_" + ascii_letters
 LDIGITS = LETTERS + digits
 TYPES = ["int", "char", "bool", "float", "double"]
 
-def process(entrie: str):
-  convert = entrie.split()
-  return convert[0], "".join(convert[1:])
-
-def automato(entrie: tuple[str, str]):
-  def automato(entrie: str):
+def autômato(entrie: str):
+  def autômato(entrie: str):
     INICIO, FINAL = 1, 3
-    def automato(state: int, word: str):
+    def autômato(state: int, word: str):
       if state == 1 and word in LETTERS: return 2
       if state == 2 and word == ",": return 1
       if state == 2 and word in LDIGITS: return 2
@@ -20,13 +16,15 @@ def automato(entrie: tuple[str, str]):
       return 0
     state = INICIO
     for word in entrie:
-      state = automato(state, word)
+      state = autômato(state, word)
     return True if state == FINAL else False
-  return entrie[0] in TYPES and automato(entrie[1])
+  conver = entrie.split()
+  conver = conver[0], "".join(conver[1:])
+  return conver[0] in TYPES and autômato(conver[1])
 
 def boolean(entrie: bool):
   return "aceito" if entrie else "recusado"
 
 system("cls||clear")
-resul = automato(process(input("Entrada: ")))
+resul = autômato(input("Entrada: "))
 print(f"O conteúdo foi {boolean(resul)}!")
