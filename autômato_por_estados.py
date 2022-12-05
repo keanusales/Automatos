c81ddbe4603f15143293623d5194bb2f323e5e93
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2022-12-05 13:36:52 -0300

    Update autômato_por_estados.py

--- autômato_por_estados.py
+++ autômato_por_estados.py
@@ -11,16 +11,17 @@ def process(entrie: str):
 
 def automato(entrie: tuple[str, str]):
   def automato(entrie: str):
+    INICIO, FINAL = 1, 3
     def automato(state: int, word: str):
       if state == 1 and word in LETTERS: return 2
       if state == 2 and word == ",": return 1
       if state == 2 and word in LDIGITS: return 2
       if state == 2 and word == ";": return 3
       return 0
-    state = 1 # O estado inicial é 1
+    state = INICIO
     for word in entrie:
       state = automato(state, word)
-    return True if state == 3 else False
+    return True if state == FINAL else False
   return entrie[0] in TYPES and automato(entrie[1])
 
 def boolean(entrie: bool):
