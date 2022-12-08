68b7ba73c524ab52dd2a69c624a10aa93ac24f64
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2022-12-08 19:14:38 -0300

    Create automato03v3.py

---
+++ automato03v3.py
@@ -0,0 +1,56 @@
+from os import system
+from itertools import product
+from string import ascii_letters, digits
+
+LETTERS = "_" + ascii_letters
+LDIGITS = LETTERS + digits
+TYPES = ["int", "char", "bool", "float", "double"]
+
+def verifyExclusives(entrie: list[str]):
+  for type, elem in product(TYPES, entrie):
+    if type in elem: return False
+  return True
+
+def autômato(entrie: str):
+  everyword = []
+  def autômato(entrie: list[str]):
+    INICIO, INTER, FINAL = 1, 1, 3
+    def autômato(state: int, word: str):
+      if state == 1 and word in LETTERS: return 2
+      if state == 2 and word == ",": return 1
+      if state == 2 and word in LDIGITS: return 2
+      if state == 2 and word == ";": return 3
+      return 0
+    state = INICIO
+    for word in entrie[:-1]:
+      for carac in word:
+        state = autômato(state, carac)
+      everyword.append((word[:-1], state == INTER))
+    word = entrie[-1]
+    for carac in word:
+      state = autômato(state, carac)
+    everyword.append((word[:-1], state == FINAL))
+    return state == FINAL
+  convert = entrie.split()
+  convert = convert[0], convert[1:]
+  resul = verifyExclusives(convert[1])
+  resul = resul and convert[0] in TYPES
+  resul = resul and autômato(convert[1])
+  return resul, convert[0], everyword
+
+def separationTypes(entrie: str):
+  lista = entrie.split(";")
+  return [f"{elem};" for elem in lista if elem != ""]
+
+def boolean(entrie: bool):
+  return "aceito(a)" if entrie else "recusado(a)"
+
+system("cls||clear")
+entrie = input("Entrada: ")
+entrie = separationTypes(entrie)
+for elem in entrie:
+  resul = autômato(elem)
+  print(f"\nO conteúdo foi {boolean(resul[0])}!")
+  print(f"As variáveis são do tipo {resul[1]}.")
+  for word in resul[2]:
+    print(f"\"{word[0]}\" foi {boolean(word[1])}")
